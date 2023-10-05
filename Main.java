3d45801d4a3a873b8cefe1e5b54d0ea9cf007778
FMR08 <135428100+FMR08@users.noreply.github.com>
2023-10-05 17:52:14 -0300

    Update Main.java
    avance en la practica

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Date;
 
 class Cliente {
@@ -12,6 +13,25 @@ class Direcion {
     private String direccion;
     public Direcion(String direc) {
         direccion = direc;
+    }
+}
+class DocTributario{
+    private String numero;
+    private String rut;
+    private Date fecha;
+    public DocTributario(String num, String rut){
+        this.rut=rut;
+        numero=num;
+    }
+}
+class Boleta extends DocTributario{
+    public Boleta(String num, String rut) {
+        super(num, rut);
+    }
+}
+class Factura extends DocTributario{
+    public Factura(String num, String rut) {
+        super(num, rut);
     }
 }
 class Articulo {
@@ -45,33 +65,71 @@ class DetalleOrden {
     }
 
     public float calcPrecio() {
-        return articulo.getPrecio() * cantidad;
+        return calcPreciosinIVA() +calcIVA();
     }
 
     public float calcPreciosinIVA() {
-        return calcPrecio() - calcIVA();
+        return articulo.getPrecio()* cantidad;
     }
 
     public float calcIVA() {
-        return cantidad * articulo.getPrecio() / 10;
+        return calcPreciosinIVA() / 10;
     }
 
     public float calcPeso() {
         return articulo.getPeso() * cantidad;
     }
 }
+class OrdenCompra{
+    private Date fecha;
+    private String estado;
+    ArrayList<DetalleOrden> Orden= new ArrayList<DetalleOrden>();
+    public OrdenCompra (String estado){
+        this.estado=estado;
+    }
+    public void AgregarCompra(DetalleOrden gg) {
+        Orden.add(gg);
+    }
+    public float calcPrecio() {
+    return 0;
+    }
+    public float calcPreciosinIVA() {
+        return 0;
+    }
+    public float calcIVA() {
+        return 0;
+    }
+    public float calcPeso() {
+        
+        return 0;
+    }
+}
 class Pago {
     private float monto;
     private Date fecha;
+
+    public Date getFecha() {
+        return fecha;
+    }
 }
 class Efectivo extends Pago {
-    public float calcDevolucion
+    public float calcDevolucion;
 }
 class Transferencia extends Pago {
+    private String banco;
+    private String numCuenta;
+    public Transferencia(String ban, String num){
+        banco=ban;
+        numCuenta=num;
+    }
 }
 class Tarjeta extends Pago {
 }
 public class Main {
+    public static void main(String[] args) {
+        Pago pago=new Pago();
+        Date fecha=new Date(5,5,5);
+        System.out.println(pago.getFecha());
 
-
+    }
 }
